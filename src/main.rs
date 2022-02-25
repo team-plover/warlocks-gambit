cd05a56706aae364d0a159df1aada0935634b190
vasukas <vasukas@yandex.ru>
2022-02-26 04:58:32 +0500

    Fix app hanging on exit on Linux

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,11 @@ mod ui;
 fn main() {
     let mut app = App::new();
     app.insert_resource(Msaa { samples: 4 })
+        .insert_resource(WindowDescriptor {
+            #[cfg(target_os = "linux")]
+            vsync: false, // workaround for https://github.com/bevyengine/bevy/issues/1908 (seems to be Mesa bug with X11 + Vulkan)
+            ..Default::default()
+        })
         .add_plugins(DefaultPlugins)
         .add_plugin(PhysicsPlugin::default())
         .add_plugin(audio::Plugin)
